c7903480373c4bf4ae593d3487e374e143093081
Aqeel <aqeelaqeelzeid@gmail.com>
2019-09-04 21:03:21 +0530

    Created a global variable for Apollo Client

--- src/index.js
+++ src/index.js
@@ -4,13 +4,17 @@ import '../src/styles/index.css'
 import App from '../src/components/App'
 import { ApolloProvider } from 'react-apollo'
 import ApolloClient from 'apollo-boost'
+import {useGlobal} from 'reactn'
 
 const client = new ApolloClient({
   uri: 'https://nuhaprismadb-e9e96b51e5.herokuapp.com/nuha-graphql/dev',
 
 })
 
-//Apollo Client
+//Apollo Client Set Up to be Accessed from anywhere
+const [myApolloClient, setMyApolloClient] = useGlobal(client)
+
+
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
